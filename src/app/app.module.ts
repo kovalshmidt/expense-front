7d1a4b1305fd4bd6bb7c25d31469f04b00ddb5b8
mineo13 <shkoloto1@gmail.com>
2020-02-22 23:54:40 +0200

    Created Routing and usable navbar . Resolve #9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ExpenseComponent } from './expense/expense.component';
 import { CategoryComponent } from './category/category.component'
+import {Router, RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes :Routes = [
+  {
+    path:'expense',
+    component:ExpenseComponent
+  },
+  {
+    path:'categories',
+    component:CategoryComponent
+  },
+  {
+    path:'',
+    component:ExpenseComponent,
+    pathMatch:'full'
+  },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +35,9 @@ import { CategoryComponent } from './category/category.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
