b2395d3dd007367cf3ffc5d4f93dc357b36a5102
mineo13 <shkoloto1@gmail.com>
2020-02-29 19:58:26 +0200

    Created Expenses API functionality.Resolve #10.

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Expense} from "../model/expense";
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +22,27 @@ export class ApiService {
   private PUT_CATEGORY_URL = `${this.BASE_URL}//category//`;
   private DELETE_CATEGORY_URL = `${this.BASE_URL}/category//`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAllExpenses(): Observable<Expense[]> {
+    return this.http.get<Expense[]>(this.GET_ALL_EXPENSES_URL);
+  }
+
+  getExpenseById(id: string): Observable<Expense[]> {
+    return this.http.get<Expense[]>(this.GET_EXPENSE_BY_ID + id);
+  }
+
+  updateExpense(expense: Expense): Observable<Expense> {
+    return this.http.put<Expense>(this.POST_EXPENSE_URL, expense);
+  }
+
+  deleteExpense(id: string): Observable<any> {
+    return this.http.delete(this.DELETE_EXPENSE_URL + id);
+  }
+
+  saveExpense(expense: Expense): Observable<Expense> {
+    return this.http.post<Expense>(this.PUT_EXPENSE_URL, expense);
+  }
+
 }
