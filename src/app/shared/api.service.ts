c7b947223bbeadf415e31d3797a68650bac2c443
vitaliy <vkovalevskiy@omilia.com>
2020-02-29 11:05:32 +0200

    Renamed ApiService

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceService {
+export class ApiService {
 
   private BASE_URL = `http://localhost:8080` + `//api`;
   // Expense
