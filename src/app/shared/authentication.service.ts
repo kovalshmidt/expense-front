ef6c81f3dc6bba89e9f20f479be38953eb84c578
vitaliy <vkovalevskiy@omilia.com>
2020-05-17 09:25:44 +0300

    Added methods in Authentication

--- src/app/shared/authentication.service.ts
+++ src/app/shared/authentication.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
+
+export const TOKEN_NAME = 'jwt_token';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +20,40 @@ export class AuthenticationService {
   }
 
   isUserLoggedIn() {
-    const jwt = localStorage.getItem('token');
-    return !(jwt === null);
+    const jwt = localStorage.getItem(TOKEN_NAME);
+    return !(jwt === null) && !this.isTokenExpired(jwt);
   }
 
   logOut() {
-    localStorage.removeItem('token');
+    localStorage.removeItem(TOKEN_NAME);
+  }
+
+  getToken(): string {
+    return localStorage.getItem(TOKEN_NAME);
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem(TOKEN_NAME, token);
+  }
+
+  getTokenExpirationDate(token: string): Date {
+    const decoded = jwt_decode(token);
+
+    if (decoded.exp === undefined) {
+      return null;
+    }
+
+    const date = new Date(0);
+    date.setUTCSeconds(decoded.exp);
+    return date;
+  }
+
+  isTokenExpired(token?: string): boolean {
+    if (!token) { token = this.getToken(); }
+    if (!token) { return true; }
+
+    const date = this.getTokenExpirationDate(token);
+    if (date === undefined) { return false; }
+    return !(date.valueOf() > new Date().valueOf());
   }
 }
