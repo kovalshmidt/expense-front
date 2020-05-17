4626a254e81ad12b8f8ce58c0d96fdf79c72a68b
vitaliy <vkovalevskiy@omilia.com>
2020-05-17 09:32:16 +0300

    added interceptor to add Authorization header to the requests based on the token

--- src/app/shared/httpinterceptor.service.ts
+++ src/app/shared/httpinterceptor.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {AuthenticationService, TOKEN_NAME} from './authentication.service';
+
+const AUTH_PREFIX = 'Bearer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private auth: AuthenticationService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    if (localStorage.getItem('token')) {
+    const token = localStorage.getItem(TOKEN_NAME);
+    if (this.auth.isUserLoggedIn()) {
       req = req.clone({
         setHeaders: {
-          Authorization: localStorage.getItem('token')
+          Authorization: `${AUTH_PREFIX} ${token}`
         }
       });
     }
